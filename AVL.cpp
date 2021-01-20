adb875c5de15b9e83b5a16b14ebf5671fa2e8181
jladron <jladron@hotmail.es>
2021-01-20 20:16:44 +0100

    Añado Arboles general y termino AVL

--- AVL.cpp
+++ AVL.cpp
@@ -27,7 +27,135 @@ info_nodo_AVL<T>* Busqueda(info_nodo<T>*n, const T &x){
             }
         }
     }
+};
+
+
+//En un AVL en la inserción debemos mantener las alturas de los nodos 
+//Se distinguen 4 casos
+//Caso A: ROTACIÓN SIMPLE a DERECHA (RSD) sucede cuando se inserta un 
+// nuevo nodo como hijo a la izquierda
+
+template <class T>
+void SimpleDerecha(info_nodo_AVL<T> * & n){
+    info_nodo_AVL<T> * aux = n->hizq;
+    info_nodo_AVL<T> * padre = n->padre;
+    n->hizq = n->hder;
+    if(n->hizq != nullptr){
+        n->hizq->padre = n;
+    }
+    n->padre = aux;
+    aux->padre = padre;
+    aux->hder = n;
+    n = aux;
+    ActualizarAltura(n->hder);
+};
+
+template <class T>
+void ActualizarAltura(info_nodo_AVL<T>* &n){
+    if(n != 0){
+        n->altura = std::max(Altura(n->hizq),
+                    Altura(n->hder)) + 1;
+        ActualizarAltura(n->padre);    
+    }
+};
+
+
+// Caso D_ ROTACIÓN SIMPLE a IZQUIERDA(RSI) sucede cunado insertamos un 
+// nuevo nodo a la derecha 
+
+template <class T>
+void SimpleIzquierda(info_nodo_AVL<T>* &n){
+    info_nodo_AVL<T> * aux = n->hder;
+    info_nodo_AVL<T> * padre = n->padre;
+    n->hder = n->hizq;
+    if(n->hder != nullptr){
+        n->hder->padre = n;
+    }
+    n->padre = aux;
+    aux->padre = padre;
+    aux->hizq = n;
+    n = aux;
+    ActualizarAltura(n->hizq);
+};
+
+// Caso B debemos realizar una rotación simple a la izquierda
+// sobre el hizq del nodo donde se produce el desequilibrio y luego 
+// realizamos una rotación simple a la derecha sobre el nodo donde se produce 
+// el desquilibrio
+
+template <class T>
+void Doble_IzquierdaDerecha(info_nodo_AVL<T>* & n){
+    SimpleIzquierda(n->hizq);
+    SimpleDerecha(n);
+};
+
+// Caso C: debemos realizar primero una rotacion simple a la derecha 
+// sobre el hijo a la derecha del nodo donde se produce el desequilibrio
+// y luego una rotación simple a la izquierda en el nodo 
+
+template <class T>
+void Doble_DerechaIzquierda(info_nodo_AVL<T>* & n){
+    SimpleDerecha(n->hder);
+    SimpleIzquierda(n);
 }
+
+//Ahora que tenemos todos los posibles desequilibrios en mente procedemos
+// con el método de inserción
+
+template<class T>
+bool InsertarAVL(info_nodo_AVL<T>* &raiz, T x){
+    if(raiz == nullptr){
+        raiz = new info_nodo_AVL<T>(x);
+        raiz->altura = 0;
+        return true;
+    }else{
+        if(x < raiz->et){
+            if(InsertarAVL(raiz->hizq, x)){
+                raiz->hizq->padre = raiz;
+            }
+            switch (Altura(raiz->hizq) - Altura(raiz->padre)){
+                case 0: return false;
+                case 1:
+                    raiz->altura++;
+                    return true;
+                case 2:
+                    //CASO A
+                if(Altura(raiz->hizq->hizq) > Altura(raiz->hizq->hder)){
+                    SimpleDerecha(raiz);
+                }else{
+                    //CASO B
+                    Doble_IzquierdaDerecha(raiz);
+                }
+                return false;
+            }
+        }else{ //x es mayor que la etiqueta de la raiz
+            if(InsertarAVL(raiz->hder, x){
+                if(raiz->hder != nullptr){
+                    raiz->hder->padre = raiz;
+                }
+
+                switch (Altura(raiz->heder) - Altura(raiz->hizq)){
+                    case 0: 
+                        return false;
+                    case 1: 
+                        raiz->altura++;
+                        return true;
+                    case 2:
+                        //CASO D
+                        if(Altura(raiz->hder->hder) > Altura(raiz->hder->hizq)){
+                            SimpleIzquierda(raiz);
+                        }else{
+                            //CASO C
+                            Doble_DerechaIzquierda(raiz);
+                        }
+                        return false;
+                }
+            }
+        }
+    }
+
+}
+
 
 int main(){
 
---
+++ Arboles.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+//Arboles generales
+
+template <class T>
+struct info_nodo{
+    T et;
+    info_nodo<T>* padre, *hijoizq, *hermanodcha;
+    info_nodo(){
+        padre=
+        hijoizq=
+        hermanodcha = 0;
+    }
+    info_nodo(const T & e){
+        et = e;
+        padre = 
+        hijoizq = 
+        hermanodcha = 0;
+    }
+}
+
+template <class T>
+info_nodo<T>* CrearRaiz(const T &e){
+    info_nodo<T> * n = new info_nodo<T>(e);
+}
+
+template <class T>
+void Copiar(info_nodo<T>*s, info_nodo<T>* &d){
+    if(s == nullptr){
+        d = nullptr;
+    }else{
+        d = CrearRaiz(s->et);
+        Copiar(s->hijoizq, d->hijoizq);
+        Copiar(s->hermanodcha, d->hermanodcha);
+        if(d->hijoizq != 0){
+            d->hijoizq->padre = d;
+        }
+
+        if(d->hermanodcha != 0){
+            d->hermanodcha->padre = d->padre;
+        }
+    }
+}
+
+
+template <class T>
+void Borrar(info_nodo<T>*s){
+    if(s != nullptr){
+        Borrar(s->hijoizq);
+        Borrar(s->hermanodcha);
+        delete s;
+    }
+}
+
+template <class T>
+void InsertarHijoIzq(info_nodo<T>*n, info_nodo<T>* & T2){
+    if(T2 != nullptr){
+        T2->hermanodcha = n->hijoizq;
+        T2->padre = n;
+        n->hijoizq = T2;
+        T2 = nullptr;
+    }
+}
+
+
+template<class T>
+void InsertarHermanoDrcha(info_nodo<T>*n, info_nodo<T>* & T2){
+    if(T2 != nullptr){
+        T2->hermanodcha = n->hermanodcha;
+        T2->padre = n->padre;
+        n->hermanodcha = T2;
+        T2 = nullptr;
+    }
+}
+
+template<class T>
+info_nodo<T>* PodarHijoIzq(info_nodo<T>* n){
+    
+
+    if(n->hijoizq != nullptr){
+        info_nodo<T> * res = n->hijoizq;
+        n->hizq = n->hijoizq->hermanodcha;
+        res->padre = nullptr;
+        res->hermanodcha = nullptr;
+        return res;
+    }
+    return nullptr;
+}
+
+template<class T>
+info_nodo<T>* PodarHermanoDrcha(info_nodo<T>* n){
+    info_nodo<T> * res = 0;
+    if(n->hermanodcha){
+        res = n->hermanodcha;
+        n->hermanodcha =  res->hermanodcha;
+        res->hermanodcha = nullptr;
+        res->padre = nullptr;
+    }
+    return res;
+}
+
+int main(){
+
+    return 0;
+}
